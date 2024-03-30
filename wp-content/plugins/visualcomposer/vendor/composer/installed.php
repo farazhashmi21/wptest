768975f2209e8d26cf0c078c26d7658da8200205
farazhashmi21 <farazhashmi9000@hotmail.com>
2024-03-30 17:36:57 +0000

    Visual Composer Website Builder Plugin Added To The Project;

---
+++ wp-content/plugins/visualcomposer/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '45.8.x-dev',
+    'version' => '45.8.9999999.9999999-dev',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9ee0e5ce23d17310c62c0a9f2bd3bb114d046d29',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '45.8.x-dev',
+      'version' => '45.8.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9ee0e5ce23d17310c62c0a9f2bd3bb114d046d29',
+    ),
+  ),
+);
